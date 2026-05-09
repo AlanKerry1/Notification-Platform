813dd8e06c0c2cceb7ed0d563fca4f580978ec79
Alan Kerry <alankerry468@gmail.com>
2026-05-09 14:31:13 +0300

    organized fs according to clean architect in producer

---
+++ producer/src/notification/infrastructure/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { RabbitmqNotificationQueue } from './rabbitmq.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: []
+})
+export class RabbitmqModule {}
---
+++ producer/src/notification/notification.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RabbitmqModule } from './infrastructure/rabbitmq/rabbitmq.module';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class NotificationModule {}
